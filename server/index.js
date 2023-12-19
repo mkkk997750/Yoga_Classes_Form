d38dc1ac694454cd7565432d4e2f2580c13fee4b
mkkk997750 <eemukeshshah@gmail.com>
2023-12-20 03:59:31 +0530

    mkkk

--- server/index.js
+++ server/index.js
@@ -13,6 +13,152 @@
 // });
 
 
+// const express = require("express");
+// const db = require('./db');
+// const cors = require('cors');
+
+// const PORT = process.env.PORT || 3001;
+
+// const app = express();
+
+// app.use(cors());
+// app.use(express.json());
+
+// app.post('/api/enroll', (req, res) => {
+//     // Get the user data from the request body
+//     const { name, email, age, batch, month } = req.body;
+  
+//     // Validate the user data
+//     if (!name || !email || !age || !batch || !month) {
+//       return res.status(400).json({ message: 'Please fill all the fields' });
+//     }
+  
+//     if (age < 18 || age > 65) {
+//       return res.status(400).json({ message: 'Age limit is 18-65' });
+//     }
+  
+//     // Check if the user already exists in the database
+//     db.query('SELECT * FROM users WHERE email = ?', [email], (err, result) => {
+//       if (err) {
+//         console.log(err);
+//       } else {
+//         if (result.length > 0) {
+//           // User already exists, return an error
+//           return res.status(400).json({ message: 'User already exists' });
+//         } else {
+//           // User does not exist, insert the user data into the database
+//           db.query(
+//             'INSERT INTO users (name, email, age, batch, month, payment_status) VALUES (?,?,?,?,?,?)',
+//             [name, email, age, batch, month, 'pending'],
+//             (err, result) => {
+//               if (err) {
+//                 console.log(err);
+//               } else {
+//                 // User data inserted successfully, return a success message
+//                 return res.status(200).json({ message: 'User enrolled successfully' });
+//               }
+//             }
+//           );
+//         }
+//       }
+//     });
+//   });
+  
+//   // Route for getting the user data by their email
+//   app.get('/api/user/:email', (req, res) => {
+//     // Get the email from the request params
+//     const email = req.params.email;
+  
+//     // Query the database for the user data by their email
+//     db.query('SELECT * FROM users WHERE email = ?', [email], (err, result) => {
+//       if (err) {
+//         console.log(err);
+//       } else {
+//         if (result.length > 0) {
+//           // User data found, return the user data
+//           return res.status(200).json(result[0]);
+//         } else {
+//           // User data not found, return an error
+//           return res.status(404).json({ message: 'User not found' });
+//         }
+//       }
+//     });
+//   });
+  
+//   // Route for updating the user data and changing their batch
+//   app.put('/api/user/:email', (req, res) => {
+//     // Get the email from the request params
+//     const email = req.params.email;
+  
+//     // Get the updated user data from the request body
+//     const { name, age, batch, month } = req.body;
+  
+//     // Validate the updated user data
+//     if (!name || !age || !batch || !month) {
+//       return res.status(400).json({ message: 'Please fill all the fields' });
+//     }
+  
+//     if (age < 18 || age > 65) {
+//       return res.status(400).json({ message: 'Age limit is 18-65' });
+//     }
+  
+//     // Query the database to update the user data by their email
+//     db.query(
+//       'UPDATE users SET name = ?, age = ?, batch = ?, month = ? WHERE email = ?',
+//       [name, age, batch, month, email],
+//       (err, result) => {
+//         if (err) {
+//           console.log(err);
+//         } else {
+//           // User data updated successfully, return a success message
+//           return res.status(200).json({ message: 'User updated successfully' });
+//         }
+//       }
+//     );
+//   });
+  
+//   // Route for processing the payment using the CompletePayment() function
+//   app.post('/api/payment/:email', (req, res) => {
+//     // Get the email from the request params
+//     const email = req.params.email;
+  
+//     // Get the payment details from the request body
+//     const { cardNumber, expiryDate, cvv, amount } = req.body;
+  
+//     // Validate the payment details
+//     if (!cardNumber || !expiryDate || !cvv || !amount) {
+//       return res.status(400).json({ message: 'Please fill all the fields' });
+//     }
+  
+//     // Call the CompletePayment() function with the payment details
+//     CompletePayment(cardNumber, expiryDate, cvv, amount, (err, result) => {
+//       if (err) {
+//         // Payment failed, return an error
+//         return res.status(400).json({ message: 'Payment failed' });
+//       } else {
+//         // Payment succeeded, update the payment status in the database
+//         db.query(
+//           'UPDATE users SET payment_status = ? WHERE email = ?',
+//           ['paid', email],
+//           (err, result) => {
+//             if (err) {
+//               console.log(err);
+//             } else {
+//               // Payment status updated successfully, return a success message
+//               return res.status(200).json({ message: 'Payment successful' });
+//             }
+//           }
+//         );
+//       }
+//     });
+//   });
+
+// app.listen(PORT, () => {
+//   console.log(`Server listening on ${PORT}`);
+// });
+
+
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
@@ -21,9 +167,10 @@ const jwt = require('jsonwebtoken');
 const request = require('request');
 
 const app = express();
-
+// mongodb://0.0.0.0:27017/yoga
+// mongodb+srv://tropnicates:BQZ7PIxJHKdroqex@fullstack-blog-project.otgvrqz.mongodb.net/?retryWrites=true&w=majority
 // Connect to MongoDB database
-mongoose.connect('mongodb+srv://tropnicates:BQZ7PIxJHKdroqex@fullstack-blog-project.otgvrqz.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true,  useUnifiedTopology: true
+mongoose.connect('mongodb://0.0.0.0:27017/yoga_classes', { useNewUrlParser: true,  useUnifiedTopology: true
 // useFindAndModify: false 
 });
 const db = mongoose.connection;
